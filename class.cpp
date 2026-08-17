12c8221f16341199522f41cccdf60cbff20c29f9
kashish <kashishjain16263@gmail.com>
2026-08-17 11:52:30 +0530

    class_basic

--- class.cpp
+++ class.cpp
@@ -5,17 +5,15 @@ public:
     string name;
     int rollno;
     int marks;
-
     void display(){
-        cout<<name<<" "<<rollno<<" "<<marks<<endl;
-
+        cout<<"name is:"<<name<<" "<<"roll no. is:"<<rollno<<" "<<"marks:"<<marks<<endl;
     }  
-
 };
 int main(){
     Students s1,s2;
+    cout<<"Enter name, rollno and marks of student 1: ";
     cin>>s1.name>>s1.rollno>>s1.marks;
-    
+    cout<<"Enter name, rollno and marks of student 2: ";
     cin>>s2.name>>s2.rollno>>s2.marks;
 
 
